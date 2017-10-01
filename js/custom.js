413c1f299c7c33c1964fe39705247cb78c9b298d
Renanpinto <31354349+Renanpinto@users.noreply.github.com>
2017-10-01 16:56:38 -0300

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+/* NAV CHANGE*/
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 80) {
+    $(".navbar-brand img").removeClass("img-brand");
+    $(".navbar-brand img").addClass("new-brand");
+
+    $("nav").removeClass("transparent");
+    $("nav").addClass("nav-flat");
+    $("nav").addClass("fixed-top");
+
+  } else {
+    $(".navbar-brand img").removeClass("new-brand");
+    $(".navbar-brand img").addClass("img-brand");
+
+    $("nav").removeClass("nav-flat");
+    $("nav").addClass("transparent");
+    $("nav").removeClass("fixed-top");
+  }
+});
